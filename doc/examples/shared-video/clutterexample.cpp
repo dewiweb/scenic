245c82873257ceb77155f70da07f206e0e3498af
Vijay Rudraraju <veejayee@gmail.com>
2011-06-23 13:28:29 -0400

    clutter and osg versions of app that simply displays sharedvideobuffer both work now

--- doc/examples/shared-video/clutterexample.cpp
+++ doc/examples/shared-video/clutterexample.cpp
@@ -16,16 +16,12 @@
 
 #define UNUSED(x) ((void) (x))
 
-//static int __aRBLookUpTable[32] = {5,18,29,38,48,56,64,71,78,85,92,99,106,113,119,125,131,137,144,151,157,164,171,177,184,192,200,208,216,226,238,250};
-//static int __aGLookUpTable[64] = {3,10,16,22,27,32,36,41,46,50,54,57,61,65,69,73,77,80,83,86,90,93,96,100,103,106,109,113,116,119,123,126,130,133,136,139,142,145,149,152,155,158,162,165,168,172,175,178,182,185,189,193,196,200,205,209,213,218,222,227,233,239,246,252};
-
 /// FUCKING GROSS!!!
 static int GLOBAL_width = 0;
 static int GLOBAL_height = 0;
 int framecount = 0;
 
-static void key_event_cb(ClutterActor *actor, ClutterKeyEvent *event, gpointer data)
-{
+static void key_event_cb(ClutterActor *actor, ClutterKeyEvent *event, gpointer data) {
     switch (event->keyval)
     {
         case CLUTTER_Escape:
@@ -39,15 +35,19 @@ static void key_event_cb(ClutterActor *actor, ClutterKeyEvent *event, gpointer d
     }
 }
 
-static void on_frame_cb(ClutterTimeline *timeline, guint *ms, gpointer data)
-{ 
-    //SharedVideoPlayer *player = (SharedVideoPlayer*)&data;
+static void on_frame_cb(ClutterTimeline *timeline, guint *ms, gpointer data) { 
     boost::mutex::scoped_lock displayLock(((SharedVideoPlayer*)data)->displayMutex_);
 
-    bool feedback = clutter_texture_set_from_rgb_data(CLUTTER_TEXTURE(((SharedVideoPlayer*)data)->texture),((SharedVideoPlayer*)data)->pixels,false,GLOBAL_width,GLOBAL_height,0,3,CLUTTER_TEXTURE_NONE,NULL);
-    if (!feedback) {
-        std::cout << "failed" << std::endl;
-    }
+    CoglHandle new_texture = COGL_INVALID_HANDLE;
+    CoglTextureFlags flags = COGL_TEXTURE_NONE;
+    new_texture = cogl_texture_new_from_data(GLOBAL_width,GLOBAL_height,
+            flags,
+            COGL_PIXEL_FORMAT_RGB_565,
+            COGL_PIXEL_FORMAT_ANY,
+            0,
+            ((SharedVideoPlayer*)data)->pixels);
+    clutter_texture_set_cogl_texture(CLUTTER_TEXTURE(((SharedVideoPlayer*)data)->texture), new_texture);
+    cogl_handle_unref(new_texture);
 
     UNUSED(timeline);
     UNUSED(ms);
@@ -57,8 +57,7 @@ static void on_frame_cb(ClutterTimeline *timeline, guint *ms, gpointer data)
     ((SharedVideoPlayer*)data)->textureUploadedCondition_.notify_one();
 }
 
-void SharedVideoPlayer::consumeFrame(SharedVideoBuffer *sharedBuffer)
-{
+void SharedVideoPlayer::consumeFrame(SharedVideoBuffer *sharedBuffer) {
     using boost::interprocess::scoped_lock;
     using boost::interprocess::interprocess_mutex;
     using boost::interprocess::shared_memory_object;
@@ -71,37 +70,28 @@ void SharedVideoPlayer::consumeFrame(SharedVideoBuffer *sharedBuffer)
     // make sure there's no sentinel
     {
         // Lock the mutex
-        //scoped_lock<interprocess_mutex> lock(sharedBuffer->getMutex());
-        //sharedBuffer->startPushing();   // tell appsink to give us buffers
         boost::mutex::scoped_lock displayLock(displayMutex_);
     }
 
 
-    do
-    {
+    do {
         {
             // Lock the mutex
             scoped_lock<interprocess_mutex> lock(sharedBuffer->getMutex());
 
             // wait for new buffer to be pushed if it's empty
-            //sharedBuffer->waitOnProducer(lock);
-
-            if (not sharedBuffer->waitOnProducer(lock)) 
-            {
+            if (not sharedBuffer->waitOnProducer(lock)) {
                 end_loop = true;
-            }
-            else
-            {
+            } else {
                 // got a new buffer, wait until we upload it in gl thread before notifying producer
                 {
                     boost::mutex::scoped_lock displayLock(displayMutex_);
 
-                    if (killed_)
-                    {
+                    if (killed_) {
                         end_loop = true;
-                    }
-                    else
+                    } else {
                         textureUploadedCondition_.wait(displayLock);
+                    }
                 }
 
                 // Notify the other process that the buffer status has changed
@@ -109,12 +99,9 @@ void SharedVideoPlayer::consumeFrame(SharedVideoBuffer *sharedBuffer)
             }
             // mutex is released (goes out of scope) here
         }
-    }
-    while (!end_loop);
+    } while (!end_loop);
 
     std::cout << "\nWorker thread Going out.\n";
-    // erase shared memory
-    // shared_memory_object::remove("shared_memory");
 }
 
 SharedVideoPlayer::SharedVideoPlayer() : 
@@ -128,25 +115,16 @@ void SharedVideoPlayer::init(unsigned char *pixelData) {
 
     /* Get the stage and set its size and color: */
     stage = clutter_stage_get_default();
-    //clutter_actor_set_size(stage, GLOBAL_width, GLOBAL_height);
     clutter_stage_set_color(CLUTTER_STAGE(stage),&blue); 
 
     // Create glTexture to set ClutterActor with rgb565 data
 
     // Create and add texture actor
     texture = clutter_texture_new();
-    GError* error = NULL;
-    /*
-    texture = clutter_texture_new_from_file("face.png",&error);
-    clutter_actor_set_size(texture, GLOBAL_width, GLOBAL_height);
-    */
+
     clutter_container_add_actor(CLUTTER_CONTAINER(stage), texture);
     clutter_actor_show(texture);
     clutter_actor_show(stage);
-    //UNUSED(texture);
-    if (error != NULL) {
-        std::cout << "error" << std::endl;
-    }
 
     // timeline to attach a callback for each frame that is rendered
     timeline = clutter_timeline_new(60); // ms
@@ -164,35 +142,25 @@ void SharedVideoPlayer::run() {
 }
 
 /// Called from main thread
-void SharedVideoPlayer::signalKilled()
-{
+void SharedVideoPlayer::signalKilled() {
     boost::mutex::scoped_lock displayLock(displayMutex_);
     killed_ = true;
     textureUploadedCondition_.notify_one(); // in case we're waiting in consumeFrame
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     using namespace boost::interprocess;
 
     std::string sharedMemoryId("shared_memory");
-    switch (argc)
-    {
+    switch (argc) {
         case 2: // just id
             sharedMemoryId = std::string(argv[1]);
             break;
-        default:
-            //GLOBAL_width = 640;
-            //GLOBAL_height = 480;
-            break;
     }
 
-
     bool opened = false;
-    while (!opened)
-    {
-        try
-        {
+    while (!opened) {
+        try {
             // open the already created shared memory object
             shared_memory_object shm(open_only, sharedMemoryId.c_str(), read_write);
             opened = true; 
@@ -228,24 +196,18 @@ int main(int argc, char *argv[])
             player.signalKilled(); // let worker know that the mainloop has exitted
             worker.join(); // wait for worker to end out before main thread does
             std::cout << "Main thread going out\n";
-        }
-        catch(interprocess_exception &ex)
-        {
+        } catch(interprocess_exception &ex) {
             static const char *MISSING_ERROR = "No such file or directory";
-            if (strncmp(ex.what(), MISSING_ERROR, strlen(MISSING_ERROR)) != 0)
-            {
+
+            if (strncmp(ex.what(), MISSING_ERROR, strlen(MISSING_ERROR)) != 0) {
                 shared_memory_object::remove(sharedMemoryId.c_str());
                 std::cout << "Unexpected exception: " << ex.what() << std::endl;
                 return 1;
-            }
-            else
-            {
+            } else {
                 std::cerr << "Shared buffer doesn't exist yet\n";
-                //boost::this_thread::sleep(boost::posix_time::milliseconds(30)); 
             }
         }
     }   // end while not opened
 
     return 0;
 }
-
